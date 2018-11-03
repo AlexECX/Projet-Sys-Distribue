a169ea267c5568b1153eef3ca2143aef3e60d905
AlexECX <33321263+AlexECX@users.noreply.github.com>
2018-11-02 23:34:35 -0400

    fix factureList, VoitureVendre et htmls facture

--- concessionnaire/magasin/views.py
+++ concessionnaire/magasin/views.py
@@ -13,9 +13,11 @@ class VoitureCreate(CreateView):
     model = Voiture
     fields = '__all__'
 
+
 class VoitureUpdate(UpdateView):
     model = Voiture
     fields = '__all__'
+
 
 class VoitureDelete(DeleteView):
     model = Voiture
@@ -23,29 +25,37 @@ class VoitureDelete(DeleteView):
     #http_method_names = ['POST']
     success_url = reverse_lazy('magasin:index')
 
+
 class VoitureDetail(DetailView):
     model = Voiture
     fields = '__all__'
+
 
 class VoitureList(ListView):
     model = Voiture
     context_object_name = 'voiture_list'
     fields = '__all__'
+
     def get_queryset(self):
         return Voiture.objects.filter(
             ~Q(id__in=Facture.objects.values_list('voitureVendue_id', flat=True))
         )
 
+
 class VoitureVendre(CreateView):
     model = Facture
     fields = ['nom_acheteur', 'montant']
-    
+
+    def form_valid(self, form):
+        form.instance.voitureVendue = Voiture.objects.get(pk=self.kwargs['pk'])
+        return super().form_valid(form)
+
+
 class FactureList(ListView):
-    model = Voiture
+    model = Facture
     context_object_name = 'facture_list'
     fields = '__all__'
-    def get_queryset(self):
-        return Facture.objects.all()
+
 
 class FactureDetail(DetailView):
     model = Facture
